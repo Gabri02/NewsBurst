68403273a29c9ba324cbff0f0cd9e8dfed6f608a
Gabriel Sesta <gabrielsesta3@gmail.com>
2023-05-22 23:08:14 +0200

    lavoro su main.js in corso

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 const _ = require('lodash');
 
-let newsIds = [];
-let loadedNewsCount = 0;
-
 async function getLatestNewsIds() {
+    let newsIds = [];
+    let loadedNewsCount = 0;
+
     try {
         const response = await axios.get('https://hacker-news.firebaseio.com/v0/newstories.json');
-        newsIds = response.data.slice(0, 10); // Prendi solo i primi 10 ID
+        newsIds = response.data.slice(0, 10);
 
         for (const id of newsIds) {
             const news = await getNewsDetails(id);
@@ -18,6 +18,8 @@ async function getLatestNewsIds() {
     } catch (error) {
         console.error('Errore durante il recupero degli ID delle ultime notizie:', error);
     }
+
+    return { newsIds, loadedNewsCount };
 }
 
 getLatestNewsIds();
@@ -41,16 +43,25 @@ function displayNewsDetails(news) {
 }
 
 async function loadMoreNews() {
+    loadMoreBtn.disabled = true;
+
+    const { newsIds, loadedNewsCount } = await getLatestNewsIds();
     const nextIds = _.slice(newsIds, loadedNewsCount, loadedNewsCount + 10);
-    loadedNewsCount += 10;
+
+    const loadedNews = [];
 
     for (const id of nextIds) {
         const news = await getNewsDetails(id);
         if (news) {
-            displayNewsDetails(news);
+            loadedNews.push(news);
         }
     }
-    loadMoreBtn.addEventListener('click', loadMoreNews);
+
+    loadedNews.forEach((news) => {
+        displayNewsDetails(news);
+    });
+
+    loadMoreBtn.disabled = false;
 }
 
 const loadMoreBtn = document.getElementById('loadMoreBtn');
